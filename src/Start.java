0f664bb1c512bdaff84b76e7690d1bb987684702
GigiFanariu <139012939+GigiFanariu@users.noreply.github.com>
2023-08-04 11:44:45 +0200

    completato

---
+++ src/Start.java
@@ -0,0 +1,12 @@
+public class Start {
+    // testiamo i metodi della classe wordGames....
+
+    public static void main(String[] args) {
+
+        // uso il metodo main per testare la classe wordGames.....
+
+        WordGames wordGames = new WordGames();
+        System.out.println(wordGames.addHelloWord("World!"));
+        System.out.println(wordGames.getFullName("Gigi", "Fanariu"));
+    }
+}
---
+++ src/WordGames.java
@@ -0,0 +1,9 @@
+public class WordGames {
+
+    public String addHelloWord(String word) {
+        return String.format("Hello %s", word);
+    }
+    public String getFullName(String name, String surname) {
+        return name + " " + surname;
+    }
+}
